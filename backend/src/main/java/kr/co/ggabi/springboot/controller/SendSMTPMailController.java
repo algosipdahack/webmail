ef964f6192dfdce6a228f85062b9bbf5710a33a4
NownS <kimjuicy29@gmail.com>
2021-11-10 15:54:46 +0900

    fix Autowired

--- backend/src/main/java/kr/co/ggabi/springboot/controller/SendSMTPMailController.java
+++ backend/src/main/java/kr/co/ggabi/springboot/controller/SendSMTPMailController.java
@@ -2,6 +2,7 @@ package kr.co.ggabi.springboot.controller;
 
 import kr.co.ggabi.springboot.domain.params.MailParam;
 import kr.co.ggabi.springboot.service.SendSmtpMailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class SendSMTPMailController {
 
-    @Autowired
-    private SendSmtpMailService smtpMailService;
+    private final SendSmtpMailService smtpMailService;
 
     @PostMapping("/api/mail/send")
     public Map<String, String> sendMail(@RequestBody MailParam param) {
